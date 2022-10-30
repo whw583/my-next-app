a2791b3757f6e092369c86275f36ea5361ec5fa5
wuhuiwen <583559202@qq.com>
2022-10-30 14:11:17 +0800

    aws

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-     hello world next! v2
+     hello aws
     </div>
   )
 }
